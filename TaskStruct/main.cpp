3494c22e5269b7da79cae52425725307a3a4611d
Motorina Arina <arinamotorinaaaaa@gmail.com>
2021-09-22 23:15:55 +0300

    Initial Commit

---
+++ TaskStruct/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TaskStruct
+//
+//  Created by ПК on 22.09.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
